e2838954d188a2200c22c545910943204ce043e5
Bao Ho <bao.hodang@gmail.com>
2024-12-09 10:20:29 +0100

    day 8

---
+++ day-8/part1.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const path = require("path");
+
+const currentDir = path.resolve(__dirname);
+const stringInput = fs.readFileSync(`${currentDir}/input.txt`, {
+  encoding: "utf8",
+});
+
+const characters = [];
+const rows = stringInput.split("\n");
+rows.forEach((row, j) => {
+  for (let i = 0; i < row.length; i++) {
+    const character = row[i];
+    if (character === ".") {
+      continue;
+    }
+    const found = characters.findIndex((c) => c.character === character);
+    if (found === -1) {
+      characters.push({ character, occurences: [{ x: i, y: j }] });
+    } else {
+      characters[found].occurences.push({ x: i, y: j });
+    }
+  }
+});
+
+const width = rows[0].length;
+const height = rows.length;
+
+const isInbound = (position) =>
+  position.x >= 0 &&
+  position.x < width &&
+  position.y >= 0 &&
+  position.y < height;
+
+const antiNodesSet = new Set();
+const countAntinodesForPair = (position1, position2) => {
+  const { x: x1, y: y1 } = position1;
+  const { x: x2, y: y2 } = position2;
+  const vectorDelta = { x: x2 - x1, y: y2 - y1 };
+  const antiNodes = [
+    { x: x2 + vectorDelta.x, y: y2 + vectorDelta.y },
+    { x: x1 - vectorDelta.x, y: y1 - vectorDelta.y },
+  ];
+  for (let i = 0; i < antiNodes.length; i++) {
+    const antiNode = antiNodes[i];
+    if (isInbound(antiNode)) {
+      const antiNodeAsString = JSON.stringify(antiNode);
+      if (antiNodesSet.has(antiNodeAsString)) {
+        continue;
+      }
+      antiNodesSet.add(antiNodeAsString);
+    }
+  }
+};
+
+for (let i = 0; i < characters.length; i++) {
+  const { occurences } = characters[i];
+  for (let j = 0; j < occurences.length; j++) {
+    for (let k = j + 1; k < occurences.length; k++) {
+      countAntinodesForPair(occurences[j], occurences[k]);
+    }
+  }
+}
+
+console.log(antiNodesSet.size);
